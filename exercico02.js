ac31541cdb3cff90690aa694f35e40365fc840aa
SOFIA GASPARIN DE SOUZA <58981623805@imaccommand004.local>
2025-11-18 11:50:43 -0300

    finalizado exercicio 02

---
+++ exercico02.js
@@ -0,0 +1,35 @@
+let dia_semana = prompt("Infome um numero de 1 a 7: ")
+
+switch (semana) {
+    case "01":
+        console.log("Segunda-feira")
+        break;
+        
+        case "02":
+            console.log("Terça-feira")
+            break;
+
+            case "03":
+                console.log("Quarta-feira")
+                break;
+
+                 case "04":
+                console.log("Quinta-feira")
+                break;
+                
+                case "05":
+                console.log("Sexta-feira")
+                break;
+
+                 case "06":
+                console.log("Sabado")
+                break;
+
+                 case "07":
+                console.log("Domingo")
+                break;
+             
+    default: 
+    console.log("O numero informado nao e valido")
+        break;
+}
